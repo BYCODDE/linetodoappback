da3be4a0c0d9f0853b57398e20c12f4a73e408b6
BYCODDE <116018979+BYCODDE@users.noreply.github.com>
2025-03-11 01:11:16 +0400

    delete method added!

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,3 +1,4 @@
+import { ObjectId } from "mongodb";
 import { getDB } from "../db/db.js";
 
 export async function handleAllTasks(req, res) {
@@ -5,22 +6,35 @@ export async function handleAllTasks(req, res) {
   const taskCollection = db.collection("tasks");
 
   if (req.method === "GET" && req.url === "/tasks") {
+    const allTasks = await taskCollection.find().toArray();
     res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({ message: "All tasks" }));
+    res.end(JSON.stringify({ message: allTasks }));
   } else if (req.method === "POST" && req.url === "/tasks") {
     let body = "";
     req.on("data", (chunk) => {
       body += chunk;
     });
-    
 
     req.on("end", async () => {
       const newTask = JSON.parse(body);
       const result = await taskCollection.insertOne(newTask);
       res.writeHead(201, { "Content-Type": "application/json" });
-      res.end(JSON.stringify({ message: "successful inserted!" ,id:result.insertedId}));
+      res.end(
+        JSON.stringify({
+          message: "successful inserted!",
+          id: result.insertedId,
+        })
+      );
     });
-
-
+  } else if (req.method === "DELETE" && req.url.startsWith("/tasks/")) {
+    const id = req.url.split("/")[2];
+    const result = await taskCollection.deleteOne({ _id: new ObjectId(id) });
+    if (result.deletedCount === 0) {
+      res.writeHead(400, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "failed to  delete!" }));
+    } else {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "successful deleted!" }));
+    }
   }
 }
