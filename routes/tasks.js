6308a3217360bb8e89e14f0d1614f67ddae6b2cf
BYCODDE <116018979+BYCODDE@users.noreply.github.com>
2025-03-18 19:00:04 +0400

    implement error handling of routes and filtering for get tasks

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,73 +1,125 @@
-import { ObjectId } from "mongodb";
 import { getDB } from "../db/db.js";
+import { ObjectId } from "mongodb";
 
 export async function handleAllTasks(req, res) {
-  const db = await getDB();
-  const taskCollection = db.collection("tasks");
+  try {
+    const db = await getDB();
+    const taskCollection = db.collection("tasks");
 
-  if (req.method === "GET" && req.url === "/tasks") {
-    const allTasks = await taskCollection.find().toArray();
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({ message: allTasks }));
-  } else if (req.method === "POST" && req.url === "/tasks") {
-    let body = "";
-    req.on("data", (chunk) => {
-      body += chunk;
-    });
+    if (req.method === "GET" && req.url.startsWith("/tasks")) {
+      const urlParams = new URL(req.url, `http://${req.headers.host}`);
+      const filter = {};
 
-    req.on("end", async () => {
-      const newTask = JSON.parse(body);
-      const result = await taskCollection.insertOne(newTask);
-      res.writeHead(201, { "Content-Type": "application/json" });
-      res.end(
-        JSON.stringify({
-          message: "successful inserted!",
-          id: result.insertedId,
-        })
-      );
-    });
-  } else if (req.method === "DELETE" && req.url.startsWith("/tasks/")) {
-    const id = req.url.split("/")[2];
-    const result = await taskCollection.deleteOne({ _id: new ObjectId(id) });
-    if (result.deletedCount === 0) {
-      res.writeHead(400, { "Content-Type": "application/json" });
-      res.end(JSON.stringify({ message: "failed to  delete!" }));
-    } else {
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(JSON.stringify({ message: "successfully deleted!" }));
-    }
-  } else if (req.method.startsWith("PATCH") && req.url.startsWith("/tasks/")) {
-    const taskId = req.url.split("/")[2];
+      if (urlParams.searchParams.has("completed")) {
+        filter.completed = urlParams.searchParams.get("completed") === "true";
+      }
 
-    if (!ObjectId.isValid(taskId)) {
-      res.writeHead(400, {
+      if (urlParams.searchParams.has("priority")) {
+        filter.priority = parseInt(urlParams.searchParams.get("priority"), 10);
+      }
+      
+      const tasks = await taskCollection.find(filter).toArray();
+      res.writeHead(200, {
         "Content-Type": "application/json",
+        "Cache-Control": "no-cache",
       });
-      res.end(JSON.stringify({ message: "Invalid Task  ID" }));
-      return;
-    }
+      res.end(JSON.stringify(tasks));
+    } else if (req.method === "POST" && req.url === "/tasks") {
+      let body = "";
+      req.on("data", (chunck) => {
+        body += chunck.toString();
+      });
+      req.on("end", async () => {
+        const newTask = JSON.parse(body);
+        const result = await taskCollection.insertOne(newTask);
+        res.writeHead(201, {
+          "Content-Type": "application/json",
+          "Cache-Control": "no-cache",
+        });
+        res.end(
+          JSON.stringify({
+            message: "Task created successfully",
+            taskId: result.insertedId,
+          })
+        );
+      });
+    } else if (
+      req.method.startsWith("PATCH") &&
+      req.url.startsWith("/tasks/")
+    ) {
+      const taskId = req.url.split("/tasks/")[1];
+      if (!ObjectId.isValid(taskId)) {
+        res.writeHead(400, {
+          "Content-Type": "application/json",
+        });
+        res.end(JSON.stringify({ message: "Invalid task ID" }));
+        return;
+      }
 
-    let body = "";
+      let body = "";
+      req.on("data", (chunk) => {
+        body += chunk.toString();
+      });
+      req.on("end", async () => {
+        const updates = JSON.parse(body);
+        const result = await taskCollection.updateOne(
+          { _id: new ObjectId(taskId) },
+          { $set: updates }
+        );
 
-    req.on("data", (chunk) => {
-      body += chunk.toString();
-    });
+        if (result.matchedCount === 0) {
+          res.writeHead(404, {
+            "Content-Type": "application/json",
+          });
+          res.end(JSON.stringify({ message: "Task not found" }));
+          return;
+        } else {
+          res.writeHead(200, {
+            "Content-Type": "application/json",
+          });
+          res.end(JSON.stringify({ message: "Task updated successfully" }));
+        }
+      });
+    } else if (
+      req.method.startsWith("DELETE") &&
+      req.url.startsWith("/tasks/")
+    ) {
+      const taskId = req.url.split("/tasks/")[1];
+      if (!ObjectId.isValid(taskId)) {
+        res.writeHead(400, {
+          "Content-Type": "application/json",
+        });
+        res.end(JSON.stringify({ message: "Invalid task ID" }));
+        return;
+      }
 
-    req.on("end", async () => {
-      const updates = JSON.parse(body);
-      const result = await taskCollection.updateOne(
-        { _id: new ObjectId(taskId) },
-        { $set: updates }
-      );
+      const result = await taskCollection.deleteOne({
+        _id: new ObjectId(taskId),
+      });
 
-      if (result.matchedCount === 0) {
-        res.writeHead(404, { "Content-Type": "application/json" });
-        res.end(JSON.stringify({ message: "Task not found!" }));
+      if (result.deletedCount === 0) {
+        res.writeHead(404, {
+          "Content-Type": "application/json",
+        });
+        res.end(JSON.stringify({ message: "Task not found" }));
         return;
       } else {
-        res.writeHead(200, { "Content-Type": "application/json" });
-        res.end(JSON.stringify({ message: "Task successfully   updated!" }));
+        res.writeHead(200, {
+          "Content-Type": "application/json",
+        });
+        res.end(JSON.stringify({ message: "Task deleted successfully" }));
       }
+    } else {
+      res.writeHead(404, {
+        "Content-Type": "application/json",
+      });
+      res.end(JSON.stringify({ message: "Route not found" }));
+    }
+  } catch (err) {
+    console.error("Error handling tasks routes", err);
+    res.writeHead(500, {
+      "Content-Type": "application/json",
     });
+    res.end(JSON.stringify({ message: "Internal server error" }));
   }
 }
